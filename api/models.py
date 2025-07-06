7e362b23be7f69e2082a79cd83175757ffecea7f
Naghamesmael <nagham.esmael70@gmail.com>
2025-07-06 12:11:05 +0300

    modelserilizer

---
+++ api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    major = models.CharField(max_length=100)
+    enrollment_status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
